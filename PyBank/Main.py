d916d7ed59d21621f31806808ea55ef5d20422ef
linoleum79 <132376914+linoleum79@users.noreply.github.com>
2023-08-05 11:49:11 -0500

    Almost done!

--- PyBank/Main.py
+++ PyBank/Main.py
@@ -16,7 +16,22 @@ with open(csv_path,encoding="UTF-8") as bank_file:
     for row in csvreader:
         total_months = total_months +1
         date.append(row[0])
-        
+        profit.append(row[1])
+        total_profit = total_profit + int(row[1])
+        final_profit = int(row[1])
+        monthly_change_profits = final_profit - initial_profit
+        monthly_changes.append(monthly_change_profits)
+
+        total_change_profits = total_change_profits + monthly_change_profits
+        initial_profit = final_profit
+
+        average_change_profits =(total_change_profits/total_months) 
+
+        greatest_increase_profits = max(monthly_changes)
+        greatest_decrease_profits = min(monthly_changes)
+
+        increase_date = date[monthly_changes.index(greatest_increase_profits)]
+        decrease_date = date[monthly_changes.index(greatest_decrease_profits)]
 
 
 
@@ -24,10 +39,23 @@ with open(csv_path,encoding="UTF-8") as bank_file:
 
 
 
+print('----------------------------')
 print('Financial Analysis')
 print('----------------------------')
-print
-print
-print
-print
-print
+print("Total Months: " + str(total_months))
+print("Total Profits: " + "$" + str(total_profit))
+print("Average Change: " + "$" + str(int(average_change_profits)))
+print("Greatest Increase in Profits: " + str(increase_date) + " ($" + str(greatest_increase_profits) + ")")
+print("Greatest Decrease in Profits: " + str(decrease_date) + " ($" + str(greatest_decrease_profits)+ ")")
+print('----------------------------')
+
+with open('financial_analysis.txt', 'w') as text:
+    text.write("----------------------------------------------------------\n")
+    text.write("  Financial Analysis"+ "\n")
+    text.write("----------------------------------------------------------\n\n")
+    text.write("    Total Months: " + str(total_months) + "\n")
+    text.write("    Total Profits: " + "$" + str(total_profit) +"\n")
+    text.write("    Average Change: " + '$' + str(int(average_change_profits)) + "\n")
+    text.write("    Greatest Increase in Profits: " + str(increase_date) + " ($" + str(greatest_increase_profits) + ")\n")
+    text.write("    Greatest Decrease in Profits: " + str(decrease_date) + " ($" + str(greatest_decrease_profits) + ")\n")
+    text.write("----------------------------------------------------------\n")
